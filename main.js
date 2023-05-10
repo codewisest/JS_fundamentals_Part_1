5fa949de447ced7a384fe1d79160d0fd76027c3f
Chijioke <wisestcodes@gmail.com>
2023-05-10 12:54:57 -0400

    build string

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ window.onload = function() {
     if(js === 'amazing') console.log('JavaScript is fun');
     console.log(40 + 8 + 23 - 10);
 
-    let firstName = 'Chijioke';
-    console.log(firstName);
-    console.log(typeof typeof 23)
-
+    const firstName = 'Chijioke';
+    const lastName = 'Nwagwu';
+    const birthYear = '1987';
     const currentYear = 2040;
-    const ageWisest = currentYear - 1991;
-    const ageGloria = currentYear - 1994;
-    console.log(ageWisest, ageGloria);
+    const job = 'Web Developer';
+
+    const chijioke = `I'm ${firstName}, a ${currentYear - birthYear} old ${job}`;
+    console.log(chijioke);
+
+    
 }
