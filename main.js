8f8265924e6fb3dc018d6e39a26e38b9fddda648
Chijioke <wisestcodes@gmail.com>
2023-05-10 14:56:44 -0400

    check driving age

--- main.js
+++ main.js
@@ -5,12 +5,20 @@ window.onload = function() {
 
     const firstName = 'Chijioke';
     const lastName = 'Nwagwu';
-    const birthYear = '1987';
+    const birthYear = '2030';
     const currentYear = 2040;
     const job = 'Web Developer';
+    const age = currentYear - birthYear;
 
+    // string building
     const chijioke = `I'm ${firstName}, a ${currentYear - birthYear} old ${job}`;
     console.log(chijioke);
 
+    // conditionals
     
+    if(age >= 18) {
+        console.log(`${firstName} can start driving license 🚗`)
+    } else {
+        console.log(`${firstName} can start driving license 🚗 after ${18 - age} years`)
+    }
 }
