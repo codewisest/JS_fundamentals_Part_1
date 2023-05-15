7b735625b264c18260550a6c45cf945405ed005a
Chijioke <wisestcodes@gmail.com>
2023-05-15 03:56:09 -0400

    make juice

--- fundamentals2.js
+++ fundamentals2.js
@@ -7,3 +7,13 @@ function logger() {
 logger();
 logger();
 logger();
+
+function fruitProcessor(apple, orange) {
+    console.log(apple, orange);
+    const juice = `Juice with ${apple} apples and ${orange} oranges`;
+
+    return juice;
+}
+
+const fruitJuice = fruitProcessor(2, 4);
+console.log(fruitJuice);
