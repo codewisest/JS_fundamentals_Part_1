4ec7254a5fac3d133652b1c0b6f491e91d41e23c
Chijioke <wisestcodes@gmail.com>
2023-05-16 07:32:22 -0400

    calculate retirement

--- fundamentals2.js
+++ fundamentals2.js
@@ -17,3 +17,24 @@ function fruitProcessor(apple, orange) {
 
 const fruitJuice = fruitProcessor(2, 4);
 console.log(fruitJuice);
+
+// function expression
+const calcAge = function(birthYear) {
+    return 2023 - birthYear;
+}
+
+console.log(calcAge(1987));
+
+const arrowCalcAge = birthYear => 2023 - birthYear;
+
+console.log(arrowCalcAge(1989));
+
+const yearsUntilRetirement = birthYear => {
+    const age = 2023 - birthYear;
+    const retirementAge = 65;
+    const yearsLeft = retirementAge - age;
+
+    return `You have ${yearsLeft} years left before you retire`;
+}
+
+console.log(yearsUntilRetirement(1987));
