66fed7e810af2f6f8941cb26c1a166abbc1831a5
Chijioke <wisestcodes@gmail.com>
2023-05-17 02:13:35 -0400

    calculate tips

---
+++ cc2_2.js
@@ -0,0 +1,12 @@
+'use strict';
+const bills = [125, 555, 44]
+function calcTip(billValue) {
+    let tipPercent = (billValue >= 50 && billValue <=300) ? 0.15 : 0.20;
+    let tip = billValue * tipPercent;
+    return tip;
+}
+
+console.log(calcTip(100));
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
