a93ecb5f0767b7f0b36a3401393867f624ba7aa4
Chijioke <wisestcodes@gmail.com>
2023-05-16 20:12:42 -0400

    use diferrent test data

--- cc2_1.js
+++ cc2_1.js
@@ -4,9 +4,9 @@ const koalaAverage = calcThreeAverage(23, 34, 27);
 const dolphinsAverage = calcThreeAverage(85, 54, 71);
 
 const checkWinner = function(dolphinsAverage, koalaAverage) {
-    if(dolphinsAverage > koalaAverage * 2) {
+    if(dolphinsAverage >= koalaAverage * 2) {
         return `Dolphins win (${dolphinsAverage} vs ${koalaAverage})`;
-    } else if(koalaAverage . dolphinsAverage * 2) {
+    } else if(koalaAverage >= dolphinsAverage * 2) {
         return `Koalas win (${koalaAverage} vs ${dolphinsAverage})`;
     } else {
         return 'No winner yet!!!'
