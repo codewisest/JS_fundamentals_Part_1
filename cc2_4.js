3fa4c715236db325ce2ec692883348e166b30f56
Chijioke <wisestcodes@gmail.com>
2023-05-19 14:01:50 -0400

    calculate array average

--- cc2_4.js
+++ cc2_4.js
@@ -2,7 +2,7 @@ const billValues = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips2 = [];
 const totals2 = [];
 
-const calcTip2 = function(billValue) {
+const calcTip2 = billValue => {
     let tipPercent = (billValue >= 50 && billValue <=300) ? 0.15 : 0.20;
     let tip = billValue * tipPercent;
     return tip;
@@ -15,3 +15,16 @@ for (let i = 0; i < billValues.length; i++) {
 }
 
 console.log(tips2, totals2);
+
+
+const myArray = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let sum = 0;
+function calcAverage(anyArray) {
+    sum += anyArray;
+}
+
+for(let i = 0; i < myArray.length; i++) {
+    calcAverage(myArray[i]);
+}
+
+console.log(sum/myArray.length);
